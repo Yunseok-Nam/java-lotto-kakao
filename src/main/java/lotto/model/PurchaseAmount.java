add9e0d00d2694e84cb9568605dc1c29262d1fa8
haebyun <haepal330@gmail.com>
2026-02-09 15:18:28 +0900

    feat: PurchaseAmount에 equals 구현 추가

--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 import lotto.LottoRules;
 
+import java.util.Objects;
+
 public class PurchaseAmount {
     private final int amount;
 
@@ -14,5 +16,17 @@ public class PurchaseAmount {
         if (amount % LottoRules.PURCHASE_UNIT != 0) {
             throw new IllegalArgumentException("[ERROR] 구입금액은 1,000원 단위여야 합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PurchaseAmount that = (PurchaseAmount) o;
+        return amount == that.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
     }
 }
