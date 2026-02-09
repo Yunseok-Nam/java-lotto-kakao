eb8344fb8fa46bb4be8cd2699b2944c83f55de6f
haebyun <haepal330@gmail.com>
2026-02-09 15:04:18 +0900

    feat: LottoNumber 구현
    - 한 자리 숫자를 의미하는 값 객체(Value Object)
    - 유효 범위: 1~45
    - 생성 시 검증을 수행하여 항상 유효한 상태만 유지

---
+++ src/main/java/model/LottoNumber.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    private final int number;
+
+    public LottoNumber(int number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(int number) {
+        if(number<1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 범위의 숫자입니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(number);
+    }
+}
