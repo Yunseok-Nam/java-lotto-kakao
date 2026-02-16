0e4825bec024df255daabaaa67f3e5e080044adc
seogi.nam <namyunseok0328@gmail.com>
2026-02-16 19:44:25 +0900

    feat: 로또 구매 정보를 관리하는 LottoPurchaseInformation 구현

---
+++ src/main/java/lotto/model/LottoPurchaseInformation.java
@@ -0,0 +1,29 @@
+package lotto.model;
+
+public final class LottoPurchaseInformation {
+	private static final String INSUFFICIENT_ERROR = "금액이 부족합니다.";
+	private static final int MIN_AUTO_LOTTO_COUNT = 0;
+
+	private final PurchaseAmount purchaseAmount;
+	private final ManualLottoCount manualLottoCount;
+
+	public LottoPurchaseInformation(PurchaseAmount purchaseAmount, ManualLottoCount manualLottoCount) {
+		if (purchaseAmount.getAutoLottoCount(manualLottoCount) < MIN_AUTO_LOTTO_COUNT) {
+			throw new IllegalArgumentException(INSUFFICIENT_ERROR);
+		}
+		this.purchaseAmount = purchaseAmount;
+		this.manualLottoCount = manualLottoCount;
+	}
+
+	public int manualLottoCount() {
+		return manualLottoCount.value();
+	}
+
+	public int autoLottoCount() {
+		return purchaseAmount.getAutoLottoCount(manualLottoCount);
+	}
+
+	public double profitRate(int totalPrize) {
+		return purchaseAmount.calculateProfitRate(totalPrize);
+	}
+}
